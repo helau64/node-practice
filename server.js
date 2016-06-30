07f1972adb74ff0e2895f46b6af7131be533bc4d
Heather <hdavidson94@gmail.com>
2016-06-30 14:37:43 +0100

    Add server practice

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var http = require('http')
+var http = require('http');
 
-var server = http.createServer(function(request, response) {
-  console.log('Got a request!');
-  response.write('Hi');
-  response.end();
-});
+// set the port for the server
+var port = process.env.PORT || 8000;
 
-server.listen(3000);
+function handler(request, response) {
+    //display 'HELLO WORLD' when the user is on the home page
+    var url = request.url; //e.g. '/'
+    if (url.length === 1) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("HELLO WORLD!");
+  }
+}
+
+http.createServer(handler).listen(port);
+
+console.log('node http server listening on http://localhost:' + port);
